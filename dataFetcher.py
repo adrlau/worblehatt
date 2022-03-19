dab55a6d55229384809f9c0c6cdab8bf3d52206c
Adrian Gunnar Lauterer <adriangl@eirin.pvv.ntnu.no>
2022-03-19 23:06:26 +0100

    added google with untested fallback to openlibrary

--- dataFetcher.py
+++ dataFetcher.py
@@ -18,10 +18,11 @@ import json
 def getIsbn():
     #TODO :scanning isbn and input
     # isbn = 9780135166307 #input gir ett isbn 10 eller 13 nummer   # kaster en error.
-    # isbn = "0801859034" #input gir ett isbn 10 eller 13 nummer
+    isbn = "0801859034" #input gir ett isbn 10 eller 13 nummer
 
-    isbn = input("scan a book or add manually") #input gir ett isbn 10 eller 13 nummer
+    # isbn = input("scan a book or add manually") #input gir ett isbn 10 eller 13 nummer
     # try:
+    ## some input validation
     if (len(isbn) == 10 or len(isbn) == 13):
         return str(isbn)
     else:
@@ -33,25 +34,39 @@ def getIsbn():
 
 def getFromApi(isbn):
     try:
-        jsonInput = json.loads(requests.get("https://openlibrary.org/isbn/"+str(isbn)+".json").text)
-    except:
-        return "error fetching data"
-    
-    try:
-        authors = jsonInput.get("authors")
-        for i in range(len(authors)):
-            authors[i] = json.loads(requests.get("https://openlibrary.org"+str(authors[i].get("key"))+".json").text).get("name") #henter navn fra api
-        authors = list(set(authors))
+        #google books api
+        jsonInput = json.load("https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn)
+
+        authors = list(set(jsonInput.get("authors")))
+        title = jsonInput.get("title") + jsonInput.get("subtitle")
+        publishDate = jsonInput.get("publishedDate")
+        numberOfPages = jsonInput.get("pageCount")
+        languages = list(set(jsonInput.get("languages")))
         
-        title = jsonInput.get("title")
+
+        if authors == None or title == None or publishDate == None or numberOfPages == 0 or languages == None:
+            # openlibrary
+            jsonInput = json.loads(requests.get("https://openlibrary.org/isbn/"+str(isbn)+".json").text)
+            if authors == None:
+                authors = jsonInput.get("authors")
+                for i in range(len(authors)):
+                    authors[i] = json.loads(requests.get("https://openlibrary.org"+str(authors[i].get("key"))+".json").text).get("name") #henter navn fra api
+                authors = list(set(authors))
+            
+            if title == None:
+                title = jsonInput.get("title")
+                
+            if publishDate == None:
+                publishDate = jsonInput.get("publish_date")
+            
+            if numberOfPages == None:
+                numberOfPages = jsonInput.get("number_of_pages")
+            
+            if languages == None:
+                languages = jsonInput.get("languages")
+                for i in range(len(languages)):
+                    languages[i] = json.loads(requests.get("https://openlibrary.org"+str(languages[i].get("key"))+".json").text).get("name")
         
-        publishDate = jsonInput.get("publish_date")
-        
-        numberOfPages = jsonInput.get("number_of_pages")
-        s
-        languages = jsonInput.get("languages")
-        for i in range(len(languages)):
-            languages[i] = json.loads(requests.get("https://openlibrary.org"+str(languages[i].get("key"))+".json").text).get("name")
         
         bookData = {
                     "authors": authors,
@@ -60,11 +75,12 @@ def getFromApi(isbn):
                     "numberOfPages": numberOfPages,
                     "languages": languages,
                     }
-        
         return bookData
-        
+
     except:
-        return "error prosessing data"
+        return "error gathering data"
+
+
 
 def pushToDatabase(bokdata):
     print("pushing to databse")
